3ab22888c00f17c28400961e59e99c65cc69a27d
Shianne Lee <shiannelee@live.ca>
2018-05-19 00:28:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,6 +23,7 @@ function submitIt(){
 return $('form').on('submit', function(){
   if($('input:first').val() === 'correct'){
     alert('Your form is going to be submitted now.');
+    return;
   }
 });
 }
